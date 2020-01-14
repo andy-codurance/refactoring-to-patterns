339a138d92cd2b65f83f26410dc9f590a6527a4e
Andrew Pattison <49647893+andy-codurance@users.noreply.github.com>
2020-01-14 14:38:52 +0000

    added chain constructors

---
+++ ChainConstructors/Before/Loan.cs
@@ -0,0 +1,63 @@
+﻿namespace ChainConstructors.Before
+{
+    using System;
+
+    class Loan
+    {
+        private readonly CapitalStrategy strategy;
+        private readonly double notional;
+        private readonly double outstanding;
+        private readonly int rating;
+        private readonly DateTime? maturity;
+        private readonly DateTime? expiry;
+
+        public Loan(double notional,
+            double outstanding,
+            int rating,
+            DateTime? expiry)
+        {
+            this.strategy = new TermROC();
+            this.notional = notional;
+            this.outstanding = outstanding;
+            this.rating = rating;
+            this.expiry = expiry;
+        }
+
+        public Loan(double notional,
+            double outstanding,
+            int rating,
+            DateTime? maturity,
+            DateTime? expiry)
+        {
+            this.strategy = new RevolvingTermROC();
+            this.notional = notional;
+            this.outstanding = outstanding;
+            this.rating = rating;
+            this.maturity = maturity;
+            this.expiry = expiry;
+        }
+        
+        public Loan(CapitalStrategy strategy,
+            double notional,
+            double outstanding,
+            int rating,
+            DateTime? maturity,
+            DateTime? expiry)
+        {
+            this.notional = notional;
+            this.outstanding = outstanding;
+            this.rating = rating;
+            this.maturity = maturity;
+            this.expiry = expiry;
+            this.strategy = strategy;
+        }
+        
+        public string TypeOfLoan()
+        {
+            if (strategy is TermROC) return "Term loan";
+            if (strategy is RevolvingTermROC) return "Revolving loan";
+            if (strategy is RCTLTermROC) return "RCTL loan";
+            throw new InvalidOperationException();
+        }
+    }
+}
