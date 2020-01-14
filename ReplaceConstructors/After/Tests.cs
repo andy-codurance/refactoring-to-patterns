85e62d9f3b7e816d3aa4f4685683779cbadf807c
Andrew Pattison <49647893+andy-codurance@users.noreply.github.com>
2020-01-14 14:42:56 +0000

    added "afters" - ready for refactoring

---
+++ ReplaceConstructors/After/Tests.cs
@@ -0,0 +1,114 @@
+﻿namespace ReplaceConstructors.After
+{
+    using System;
+    using Xunit;
+    using Xunit.Abstractions;
+
+    public class Tests
+    {
+        private readonly ITestOutputHelper outputHelper;
+
+        public Tests(ITestOutputHelper outputHelper)
+        {
+            this.outputHelper = outputHelper;
+        }
+
+        [Fact]
+        public void CreateTermLoan()
+        {
+            var loan = new Loan(0.0, 0, null);
+            Assert.Equal("Term loan", loan.TypeOfLoan());
+        }
+
+        [Fact]
+        public void CreateTermLoan2()
+        {
+            var loan = new Loan(0.0, 0, null, null);
+            Assert.Equal("Term loan", loan.TypeOfLoan());
+        }
+
+        [Fact]
+        public void CreateTermLoan3()
+        {
+            var loan = new Loan(0.0, 0.0, 0, null, null);
+            Assert.Equal("Term loan", loan.TypeOfLoan());
+        }
+
+        [Fact]
+        public void CreateTermLoan4()
+        {
+            var loan = new Loan(null, 0.0, 0, null, null);
+            Assert.Equal("Term loan", loan.TypeOfLoan());
+        }
+
+        [Fact]
+        public void CreateTermLoan5()
+        {
+            var loan = new Loan(null, 0.0, 0.0, 0, null, null);
+            Assert.Equal("Term loan", loan.TypeOfLoan());
+        }
+        
+        [Fact]
+        public void CreateRevolverLoan()
+        {
+            var loan = new Loan(0.0, 0, null, DateTime.Now);
+            Assert.Equal("Revolver loan", loan.TypeOfLoan());
+        }
+        
+        [Fact]
+        public void CreateRevolverLoan2()
+        {
+            var loan = new Loan(0.0, 0.0, 0, null, DateTime.Now);
+            Assert.Equal("Revolver loan", loan.TypeOfLoan());
+        }
+        
+        [Fact]
+        public void CreateRevolverLoan3()
+        {
+            var loan = new Loan(null, 0.0, 0, null, DateTime.Now);
+            Assert.Equal("Revolver loan", loan.TypeOfLoan());
+        }
+        
+        [Fact]
+        public void CreateRevolverLoan4()
+        {
+            var loan = new Loan(null, 0.0, 0.0, 0, null, DateTime.Now);
+            Assert.Equal("Revolver loan", loan.TypeOfLoan());
+        }
+        
+        [Fact]
+        public void CreateRCTLLoan()
+        {
+            var loan = new Loan(0.0, 0, DateTime.Now, DateTime.Now);
+            Assert.Equal("RCTL loan", loan.TypeOfLoan());
+        }
+        
+        [Fact]
+        public void CreateRCTLLoan2()
+        {
+            var loan = new Loan(0.0, 0.0, 0, DateTime.Now, DateTime.Now);
+            Assert.Equal("RCTL loan", loan.TypeOfLoan());
+        }
+        
+        [Fact]
+        public void CreateRCTLLoan3()
+        {
+            var loan = new Loan(null, 0.0, 0, DateTime.Now, DateTime.Now);
+            Assert.Equal("RCTL loan", loan.TypeOfLoan());
+        }
+        
+        [Fact]
+        public void CreateRCTLLoan4()
+        {
+            var loan = new Loan(null, 0.0, 0.0, 0, DateTime.Now, DateTime.Now);
+            Assert.Equal("RCTL loan", loan.TypeOfLoan());
+        }
+        
+        [Fact]
+        public void CreateSpecificLoan()
+        {
+            var loan = new Loan(new CapitalStrategyRevolver(), 0.0, 0.0, 0, DateTime.Now, DateTime.Now);
+            Assert.Equal("Revolver loan", loan.TypeOfLoan());
+        }
+    }
+}
